18dba3d3d717183b8c63fa1ce4ae332ae982b10a
kartikey sood <kartikeysood@rediffmail.com>
2022-05-18 15:34:16 +0530

    initial commit

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 connect();
 
 app.use(logger("dev"));
@@ -21,6 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+/////  Routes   ///////
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
