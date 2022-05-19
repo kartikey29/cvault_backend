8736fcef2a8543ea107a0fdb6a6df71120c06413
Jishan Anwar <anwarjishan321@gmail.com>
2022-05-19 15:27:06 +0530

    Jish First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const app = require("express")();
+
+// Environment Path
+require("dotenv").config({ path: "./config.env" });
+require("./db/connection");
+port = process.env.PORT;
+
+// importing Routes
+const route = require("./routes/home.route");
+const dealerRoute = require("./routes/dealer.route");
+
+// Middleware
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+<<<<<<< HEAD
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/////  Routes   ///////
+
+app.use("/transaction", transactionRoutes);
+app.use("/users", usersRouter);
+
+app.use((req, res) => {
+  return res.status(500).send({ message: "Incorret API route hit" });
+});
+
+module.exports = app;
+=======
+// Routes
+app.use("/", route);
+app.use("/get-dealer", dealerRoute);
+app.use("/create-dealer", dealerRoute);
+
+// lIstening Server
+app.listen(port, () => {
+	console.log(`Server is starting on port ${port}`);
+});
+>>>>>>> 782e211 (Jish First Commit)
