15c503216bb40d7672bdc4a688dc973db89a36df
kartikey sood <kartikeysood@rediffmail.com>
2022-05-19 16:11:05 +0530

    fixed server

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-<<<<<<< HEAD
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,17 +25,7 @@ app.use("/transaction", transactionRoutes);
 app.use("/users", usersRouter);
 
 app.use((req, res) => {
-  return res.status(500).send({ message: "Incorret API route hit" });
+	return res.status(500).send({ message: "Incorret API route hit" });
 });
 
 module.exports = app;
-=======
-// Routes
-app.use("/dealer", dealerRoute);
-app.use("/transaction", transactionRoute);
-
-// lIstening Server
-app.listen(port, () => {
-	console.log(`Server is starting on port ${port}`);
-});
->>>>>>> 782e211 (Jish First Commit)
