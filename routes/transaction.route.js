522584aa985c5647aebffece692a4a0cc2301aa8
kartikey sood <kartikeysood@rediffmail.com>
2022-05-19 16:02:57 +0530

    trans routes

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const transaction = require("../controller/transaction");
+const transaction = require("../controller/transaction.controller");
 
 /* GET home page. */
 router.get("/test", transaction.test);
