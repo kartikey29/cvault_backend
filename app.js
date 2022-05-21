459c1e66d033cb92d5c71bc8af9382b6faa87483
Jishan Anwar <anwarjishan321@gmail.com>
2022-05-21 19:13:44 +0530

    advertisement done

--- app.js
+++ app.js
@@ -8,24 +8,16 @@ port = process.env.PORT;
 // importing Routes
 const dealerRoute = require("./routes/dealer.route");
 const transactionRoute = require("./routes/transaction.route");
+const advertisementRoute = require("./routes/advertisement.route");
 
 // Middleware
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-/////  Routes   ///////
-
-app.use("/transaction", transactionRoutes);
-app.use("/users", usersRouter);
-
-app.use((req, res) => {
-	return res.status(500).send({ message: "Incorret API route hit" });
-});
+// Routes
+app.use("/dealer", dealerRoute);
+app.use("/transaction", transactionRoute);
+app.use("/advertisement", advertisementRoute);
 
 module.exports = app;
