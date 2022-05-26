6f6b0b2dab085fa2f300da5ba470b94e27f1bd71
Jishan Anwar <anwarjishan321@gmail.com>
2022-05-26 17:53:14 +0530

    Correct Route Jish

--- routes/advertisement.route.js
+++ routes/advertisement.route.js
@@ -4,7 +4,10 @@ const {
 } = require("../controller/advertisement.controller");
 const advertisementRoute = require("express").Router();
 
+/* GET Advertisement  */
 advertisementRoute.get("/get-link", getAdvert);
+
+/* Create advertisement  */
 advertisementRoute.post("/link", advertLink);
 
 module.exports = advertisementRoute;
