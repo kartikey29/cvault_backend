f4f374fe8172b0c0259441b0c98733c4a03ed0c6
kartikey sood <kartikeysood@rediffmail.com>
2022-05-26 16:11:33 +0530

    made changes in dealer schema and trans schema added trans route

--- controller/dealer.controller.js
+++ controller/dealer.controller.js
@@ -3,36 +3,34 @@ const Dealer = require("../models/dealer.model");
 
 // Dealer Post Request
 exports.createDealer = async (req, res) => {
-	try {
-		const data = req.body; /// put something later
-		const InsertDealer = await new Dealer({
-			name: data.name,
-			phone: data.phone,
-			email: data.email,
-			status: data.status,
-		});
-		await InsertDealer.save();
-		res.status(200).json({
-			success: true,
-			message: "Data Inserted Successfully",
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({
-			message: "Something Went Wrong ",
-		});
-	}
+  try {
+    const data = req.body; /// put something later
+    const InsertDealer = await new Dealer({
+      name: data.name,
+      phone: data.phone,
+      email: data.email,
+    });
+    await InsertDealer.save();
+    return res
+      .status(200)
+      .json({ message: "Data Inserted Successfully", InsertDealer });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      error,
+    });
+  }
 };
 
 //Dealer Get Request
 exports.getDealer = async (req, res) => {
-	try {
-		const readData = await Dealer.find({});
-		res.send(readData).status(200);
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({
-			message: "something went wrong",
-		});
-	}
+  try {
+    const readData = await Dealer.find({});
+    return res.send(readData).status(200);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      message: "something went wrong",
+    });
+  }
 };
--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,35 +1,52 @@
 const mongoose = require("mongoose");
 
 module.exports = mongoose.model(
-	"transaction",
-	new mongoose.Schema({
-		transctionId: {
-			type: String,
-			required: true,
-		},
-		transactionType: {
-			type: String,
-			required: true,
-		},
-		cryptoType: {
-			type: String,
-			required: true,
-		},
-		price: {
-			type: Number,
-			required: true,
-		},
-		costPrice: {
-			type: Number,
-			required: true,
-		},
-		quantity: {
-			type: Number,
-			required: true,
-		},
-		accepted: {
-			type: Boolean,
-			default: false,
-		},
-	}),
+  "transaction",
+  new mongoose.Schema({
+    transctionId: {
+      type: String,
+      required: true,
+    },
+    dealerId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Dealer",
+    },
+    customerId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Customer",
+    },
+    transactionType: {
+      type: String,
+      required: true,
+    },
+    currency: {
+      type: String,
+      enum: ["usdt", "inr"],
+      required: true,
+    },
+    cryptoType: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    costPrice: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["accepted", "cancelled", "expired", "sent"],
+      required: true,
+      default: "sent",
+    },
+  })
 );
