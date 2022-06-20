135a438fdcb7e578e20d6fc0c0aeb5eb434ef949
kartikey <kartikeysood@rediffmail.com>
2022-06-20 14:10:24 +0530

    user model changes

--- models/User.model.js
+++ models/User.model.js
@@ -2,52 +2,50 @@ const mongoose = require("mongoose");
 
 // Creating User Schema
 const userSchema = new mongoose.Schema({
-	UID: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	firstName: {
-		type: String,
-		required: true,
-	},
-	middleName: {
-		type: String,
-	},
-	lastName: {
-		type: String,
-		required: true,
-	},
-	phone: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-	},
-	isUser: {
-		type: Boolean,
-		// default: true,
-		required: true,
-	},
-	status: {
-		type: Boolean,
-		// required: true,
-		// default: null,s
-	},
-	referalCode: {
-		type: String,
-		unique: true,
-	},
-	transactions: [
-		{
-			type: mongoose.Types.ObjectId,
-			ref: "Transaction",
-		},
-	],
+  UID: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  userType: {
+    type: String,
+    enum: ["dealer", "customer"],
+    required: true,
+  },
+  active: {
+    type: Boolean,
+  },
+  referalCode: {
+    type: String,
+    unique: true,
+  },
+  transactions: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Transaction",
+    },
+  ],
 });
 
 // User Model
