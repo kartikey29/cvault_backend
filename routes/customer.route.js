1b5076038317a875a6b658d09a359d6356d6fa91
kartikey <kartikeysood@rediffmail.com>
2022-06-20 15:49:43 +0530

    made customer add route

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -7,13 +7,13 @@ const {
 const customerRoute = require("express").Router();
 
 /* GET Customer  */
-customerRoute.get("/getAllCustomer", getCustomer);
+// customerRoute.get("/getAllCustomer", getCustomer);
 
 /* POST Customer  */
 customerRoute.post("/create-customer", postCustomer);
 
-customerRoute.post("/getCustomer", findCustomer);
+// customerRoute.post("/getCustomer", findCustomer);
 
-customerRoute.delete("/deleteCustomer", deleteCustomer);
+// customerRoute.delete("/deleteCustomer", deleteCustomer);
 
 module.exports = customerRoute;
