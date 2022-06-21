59438ca2d2bae5abf9eda64b4c19ed73ea324e0a
Jishan Anwar <anwarjishan321@gmail.com>
2022-06-21 23:09:47 +0530

    fix token jish

--- models/User.model.js
+++ models/User.model.js
@@ -2,54 +2,58 @@ const mongoose = require("mongoose");
 
 // Creating User Schema
 const userSchema = new mongoose.Schema({
-  UID: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  middleName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  status: {
-    type: Boolean,
-    default: true,
-  },
-  userType: {
-    type: String,
-    enum: ["dealer", "customer"],
-    required: true,
-  },
-  active: {
-    type: Boolean,
-  },
-  referalCode: {
-    type: String,
-    // unique: true,
-  },
-  transactions: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: "Transaction",
-    },
-  ],
+	UID: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	firstName: {
+		type: String,
+		required: true,
+	},
+	middleName: {
+		type: String,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	phone: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	status: {
+		type: Boolean,
+		default: true,
+	},
+	userType: {
+		type: String,
+		enum: ["dealer", "customer"],
+		required: true,
+	},
+	token: {
+		type: String,
+		default: "",
+	},
+	active: {
+		type: Boolean,
+	},
+	referalCode: {
+		type: String,
+		// unique: true,
+	},
+	transactions: [
+		{
+			type: mongoose.Types.ObjectId,
+			ref: "Transaction",
+		},
+	],
 });
 
 // User Model
