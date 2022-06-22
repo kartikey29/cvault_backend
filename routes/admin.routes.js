6aa463de6300f95ab359fdd20e4021ba11e9375c
kartikey <kartikeysood@rediffmail.com>
2022-06-22 17:20:07 +0530

    margin don

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -8,7 +8,7 @@ const {
 
 adminRoute.post("/setMargin", setAdminMargin);
 
-adminRoute.post("/getAdminData", getAdminData);
+adminRoute.get("/getAdminData", getAdminData);
 
 adminRoute.get("/getMargin", getAdminMargin);
 module.exports = adminRoute;
