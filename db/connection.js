fa16492a8446b0da544b20cfc5fe9f81aec0c512
kartikey <kartikeysood@rediffmail.com>
2022-06-28 16:49:51 +0530

    mongouri

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 // creating Database Connection
 mongoose.connect(process.env.DATABASE, async () => {
-	try {
-		console.log("Database is Connected succesfully");
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    console.log("Database is Connected succesfully");
+  } catch (error) {
+    console.log(error);
+  }
 });
